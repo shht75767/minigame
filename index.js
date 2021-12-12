c135495330d04da61a7517f9310c8ff6055b784f
HyeonTae-No <88132674+shht75767@users.noreply.github.com>
2021-12-12 23:50:01 +0900

    minigame JS

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const gameField = document.querySelector('.game__field');
 const fieldRect = gameField.getBoundingClientRect();
 
 console.log(fieldRect);
+function init() {
+  console.log(fieldRect);
+  addItem('carrot', 5, 'carrot/img/carrot.png');
+  addItem('bug', 5, 'carrot/img/bug.pbg');
+}
+
+function addItem(className, count, imgPath) {}
+init();
